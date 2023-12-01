fcd66f8ef123756fb2a9dddab2b19a5b7809871b
Aleksey <web.ams@yandex.ru>
2023-12-01 13:29:44 +0300

    Получил данные всех товаров через find_all

--- qwe.py
+++ qwe.py
@@ -7,17 +7,25 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'lxml') # html.parser - аналог lxml
 
-data = soup.find("div", class_="w-full rounded border")
+data = soup.find_all("div", class_="w-full rounded border")
 
-name = data.find("h4")
+for i in data:
+    name = i.find("h4").text
+    price = i.find("h5").text
+    url_img = "https://scrapingclub.com" + i.find("img", class_="card-img-top img-fluid").get("src")
+
+    print(name + '\n' + price + '\n' + url_img + '\n\n')
+
+
+'''name = data.find("h4")
 
 name = data.find("h4").text
 
 price = data.find("h5").text
 
-url_img = "https://scrapingclub.com" + data.find("img", class_="card-img-top img-fluid").get("src")
+url_img = "https://scrapingclub.com" + data.find("img", class_="card-img-top img-fluid").get("src")'''
 
 
 fl = open('proba.txt','w', encoding = 'utf-8')
-print(url_img, file = fl)
+print(data, file = fl)
 fl.close()
